e1fb633b2569d2aaefdae889d8fd43a2709bcc3e
Lautaro Heredia <lauta9710@gmail.com>
2021-07-19 13:22:58 -0300

    controllers. BossController & EmployeeController files added

---
+++ springexample/src/main/java/com/lautaro/springexample/controllers/EmployeeController.java
@@ -0,0 +1,8 @@
+package com.lautaro.springexample.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+}
--- springexample/src/main/java/com/lautaro/springexample/services/EmployeeService.java
+++ springexample/src/main/java/com/lautaro/springexample/services/EmployeeService.java
@@ -5,6 +5,6 @@ import com.lautaro.springexample.models.Employee;
 import java.util.List;
 
 public interface EmployeeService extends CrudService<Employee,String> {
-    
+
     List<Employee> findByBoss(String bossId);
 }
